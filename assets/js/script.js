1a6f204c8128425b3f3efd0e4e2b3637a956a442
zafarovpolat <atuin59354081@gmail.com>
2024-12-11 16:45:57 +0300

    Almost ready order section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
-let headerMenuBtn = document.querySelector("#headerMenuBtn")
-let headerMenuClose = document.querySelector("#headerMenuBtnClose")
+let headerMenuBtn = document.querySelector(".header__menu-open")
+let headerMenuClose = document.querySelector(".header__menu-close")
+let headerMenuBcg = document.querySelector(".header__menu-bcg")
 let headerMenu = document.querySelector(".header__menu")
+let orderSumOther = document.querySelector(".order__sum-other")
+let orderSumOtherRadio = document.querySelector("#sum4")
 
 headerMenuBtn.addEventListener('click', () => {
     headerMenu.classList.add('visible')
 })
 
-headerMenuBtnClose.addEventListener('click', () => {
+headerMenuClose.addEventListener('click', () => {
     headerMenu.classList.remove('visible')
+})
+
+headerMenuBcg.addEventListener('click', () => {
+    headerMenu.classList.remove('visible')
+})
+
+orderSumOther.addEventListener('click', () => {
+    orderSumOtherRadio.checked = 'true'
+    console.log(orderSumOtherRadio);
+
 })
